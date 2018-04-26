75b57e4d95adc95b71ef5a4167ca4217a4e14276
Jack Odell <jackodellmusic@gmail.com>
2018-04-26 23:46:10 +0100

    Next turn indicator now changes when a tile is placed.

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ $(function () {
 
   var xTurn = true;
   var oTurn = false;
-  var xMark = $('td').hasClass('X');
-  var oMark = $('td').hasClass('O');
 
+  //On click of tile, replaces the innerHTML with an X or O depending who's turn it is
+  //Currently can't stop ovewrite of tile
   $('td').click(function () {
+    var xMark = isXMark();
+    var oMark = isOMark();
+    turnIndicator();
+    isXMark();
+    isOMark();
     if (xTurn) {
-      if (xMark || oMark) {
+      if (!xMark || !oMark) {
         cantPlace();
       } else {
         $(this).html('X').addClass('X');
@@ -15,8 +20,8 @@ $(function () {
         oTurn = true;
       }
     }
-    else if (oTurn) {
-      if (xMark || oMark) {
+    else {
+      if (!xMark || !oMark) {
         cantPlace();
       } else {
         $(this).html('O').addClass('O');
@@ -25,8 +30,27 @@ $(function () {
       }
     }
   });
-
+  //Alert to stop player from clicking on a tile that is filled
   function cantPlace() {
     alert("Can't place that there!");
+  }
+  //Changes the heading that shows who's turn is next
+  function turnIndicator () {
+    var xName = "it's X's turn!";
+    var oName = "it's O's turn!";
+     if (xTurn) {
+       $('.playerTurn').html(xName);
+     } else {
+       $('.playerTurn').html(oName);
+     }
+  }
+
+  function isXMark () {
+    var isXMarked = $('td').hasClass('X');
+    return true;
+  }
+  function isOMark () {
+    var isOMarked = $('td').hasClass('O');
+    return true;
   }
 });
